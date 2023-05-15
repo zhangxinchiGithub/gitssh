852d0d1b113dc06843f26ed2c4c29b2f3059a271
zhangxinchi <2916007946@qq.com>
2023-05-15 20:44:37 +0800

    91机缘

--- src/main/java/com/jy/hello.java
+++ src/main/java/com/jy/hello.java
@@ -3,6 +3,7 @@ package com.jy;
 public class hello {
     public static void main(String[] args) {
 
-        System.out.println("91张先生");
+        System.out.println("91李先生");
+        System.out.println("91李先生的继缘 来自aka龙外，bro");
     }
 }
